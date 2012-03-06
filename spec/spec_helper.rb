2eb5ff71f8eb4175e4eee2d62b8b3cd424801c52
Ezekiel Templin <zeke@templ.in>
2012-03-06 16:24:33 -0500

    Ensure we're actually using VCR for all requests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :typhoeus
   c.configure_rspec_metadata!
-  c.allow_http_connections_when_no_cassette = true
 end
 
 RSpec.configure do |config|
