7f24f0b2e2d5d101f3eee80dc68ad3da64e566f2
Ezekiel Templin <zeke@templ.in>
2012-03-06 15:16:25 -0500

    Move phpBB into forum group

--- spec/sniffles/sniffers/forum/phpbb_spec.rb
+++ spec/sniffles/sniffers/forum/phpbb_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/cms/phpbb')
+require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/forum/phpbb')
 
 describe "Sniffers::Phpbb" do
   context "phpBB Forum" do
