31b9299f2ab22f4d5f11f52f14b3893e2da8c37b
Ezekiel Templin <zeke@templ.in>
2012-03-06 21:37:02 -0500

    Add new vBulletin matching pattern * In cases where a site owner removes the meta generator tag, we'll look for vB's JS initialization routine. * Remove version parsing from meta generator tag if it isn't there.

--- lib/sniffles/sniffers/cms/vbulletin.rb
+++ lib/sniffles/sniffers/cms/vbulletin.rb
@@ -20,7 +20,7 @@ module Sniffles
       
       private
       def found?
-        !!(meta_generator_content =~ /vBulletin/)
+        !!(js_initialization? || meta_generator_content =~ /vBulletin/)
       end
       
       def parse_feed
@@ -28,11 +28,19 @@ module Sniffles
       end
       
       def parse_version
-        @output[:version] = (meta_generator_content.match(/([\d]+\.[\d]+\.[\d]+)/) ? $1 : false)
+        if meta_generator_content
+          @output[:version] = (meta_generator_content.match(/([\d]+\.[\d]+\.[\d]+)/) ? $1 : false)
+        else
+          @output[:version] = false
+        end
       end
       
       def meta_generator_content
         text_at("//meta[@name='generator']/@content")
+      end
+      
+      def js_initialization?
+        @doc.xpath("//script[contains(text(),'vBulletin_init()')]").any?
       end
     end
   end
--- spec/sniffles/sniffers/cms/vbulletin_spec.rb
+++ spec/sniffles/sniffers/cms/vbulletin_spec.rb
@@ -3,12 +3,18 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sni
 
 describe Sniffles::Sniffers::Vbulletin do
   describe "#output"  do    
-    context "vbulletin", :vcr => { :cassette_name => "vbulletin_com" } do
+    context "vbulletin w/ meta generator", :vcr => { :cassette_name => "vbulletin_com" } do
       let(:vbulletin) { described_class.new(page_body("https://www.vbulletin.com/forum/forum.php")) }
       
       it { vbulletin.output[:found].should eq true }
       it { vbulletin.output[:version].should eq "4.1.11" }
       it { vbulletin.output[:feed].should eq "https://www.vbulletin.com/forum/external.php?type=RSS2" }
+    end
+    
+    context "vbulletin w/ JS initialization", :vcr => { :cassette_name => "blackhatworld_com" } do
+      let(:vbulletin) { described_class.new(page_body("http://www.blackhatworld.com/index.php")) }
+      
+      it { vbulletin.output[:found].should eq true }
     end
     
     context "not vbulletin" do
