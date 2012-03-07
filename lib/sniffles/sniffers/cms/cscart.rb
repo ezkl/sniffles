afdb6a567e454636eb9a68d4e69da93c5c5a19b1
Ezekiel Templin <zeke@templ.in>
2012-03-06 19:05:33 -0500

    Add CS-Cart Sniffer [Closes #17]

---
+++ lib/sniffles/sniffers/cms/cscart.rb
@@ -0,0 +1,24 @@
+module Sniffles
+  module Sniffers
+    class Cscart
+      include HTML
+      
+      attr_accessor :doc
+      attr_reader :output
+      
+      def initialize(response_body)
+        @output = {}        
+        parse(response_body) && process_document
+      end
+      
+      def process_document
+        @output[:found] = found?
+      end
+      
+      private
+      def found?
+        !!(@doc.text =~ /Powered by CS\-Cart/)
+      end
+    end
+  end
+end
