b97e409ae54584ea35ae2c0a3695e270c721017b
Ezekiel Templin <zeke@templ.in>
2013-12-29 06:31:09 -0800

    Simplify require in test

--- spec/sniffles/sniffers/cms/ghost_spec.rb
+++ spec/sniffles/sniffers/cms/ghost_spec.rb
@@ -1,5 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/cms/ghost')
+require 'spec_helper'
+require 'sniffles/sniffers/cms/ghost'
 
 describe Sniffles::Sniffers::Ghost do
   describe "#output" do
