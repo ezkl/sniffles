45b56567a4037e664296eaa0fa53892b7475e70c
Ezekiel Templin <zeke@templ.in>
2013-12-29 05:05:22 -0800

    Clean Kontera sniffer

--- spec/sniffles/sniffers/advertising/kontera_spec.rb
+++ spec/sniffles/sniffers/advertising/kontera_spec.rb
@@ -1,17 +1,17 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/advertising/kontera')
+require 'spec_helper'
+require 'sniffles/sniffers/advertising/kontera'
 
 describe Sniffles::Sniffers::Kontera do
-  describe "#output", :vcr => { :cassette_name => "softpedia_com" } do    
+  describe "#output", :vcr => { :cassette_name => "lancelhoff_com" } do
     context "kontera" do
-      let(:kontera)  { described_class.new(page_body("http://www.softpedia.com/")) }
-      
+      let(:kontera)  { described_class.new(page_body("http://www.lancelhoff.com/")) }
+
       it { kontera.output[:found].should eq true }
     end
 
     context "no kontera" do
       let(:blank)   { described_class.new(empty_html_doc) }
-      
+
       it { blank.output[:found].should eq false }
     end
   end
