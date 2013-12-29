0f4bd94611ff26a54a4c1b3ae108294dd4925cff
Ezekiel Templin <zeke@templ.in>
2013-12-29 04:40:34 -0800

    Clean MixPanel sniffer

--- lib/sniffles/sniffers/analytics/mixpanel.rb
+++ lib/sniffles/sniffers/analytics/mixpanel.rb
@@ -2,22 +2,22 @@ module Sniffles
   module Sniffers
     class Mixpanel
       include Text
-      
+
       attr_accessor :doc
       attr_reader :output
-      
+
       def initialize(response_body)
-        @output = {}        
+        @output = {}
         parse(response_body) && process_document
       end
-      
+
       def process_document
         @output[:found] = found?
       end
-      
+
       private
       def found?
-        match?(/api\.mixpanel\.com\S+mixpanel\.js/)
+        match?(/api\.mixpanel\.com\/.+mixpanel\.[0-9]?\.?js/)
       end
     end
   end
