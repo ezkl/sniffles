36f776293541a2eed5c9116456f1091785a23341
Ezekiel Templin <zeke@templ.in>
2013-12-29 03:56:20 -0800

    Update Google Analytics sniffer

--- lib/sniffles/sniffers/analytics/google_analytics.rb
+++ lib/sniffles/sniffers/analytics/google_analytics.rb
@@ -2,26 +2,26 @@ module Sniffles
   module Sniffers
     class GoogleAnalytics
       include Text
-      
+
       attr_accessor :doc
       attr_reader :output
-      
+
       def initialize(response_body)
-        @output = {}        
+        @output = {}
         parse(response_body) && process_document
       end
-      
+
       def process_document
         if @output[:found] = found?
           parse_google_analytics_ua
         end
       end
-      
+
       private
       def found?
-        match?(/google\-analytics\.com\/ga\.js|urchinTracker/i)
+        match?(/google\-analytics\.com\/ga\.js/)
       end
-      
+
       def parse_google_analytics_ua
         @output[:ua] = capture(/[\"|\'](UA\-[\d]+\-[\d])[\"|\']/)
       end
--- spec/sniffles/sniffers/analytics/google_analytics_spec.rb
+++ spec/sniffles/sniffers/analytics/google_analytics_spec.rb
@@ -1,27 +1,15 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/analytics/google_analytics')
+require 'spec_helper'
+require 'sniffles/sniffers/analytics/google_analytics'
 
 describe Sniffles::Sniffers::GoogleAnalytics do
   describe "#output" do
-    context "inline js", :vcr => { :cassette_name => "pearsonified_com" } do
+    opts = { cassette_name: "pearsonified_com" }
+
+    describe "inline js", vcr: opts  do
       let(:ga) { described_class.new(page_body("http://www.pearsonified.com/")) }
-      
+
       it { ga.output[:found].should eq true }
       it { ga.output[:ua].should eq "UA-2916092-1" }
-    end
-    
-    context "inline w/ urchin", :vcr => { :cassette_name => "humemes_com" } do
-      let(:ga) { described_class.new(page_body("http://humemes.com/")) }
-      
-      it { ga.output[:found].should eq true }
-      it { ga.output[:ua].should eq "UA-386965-7" }
-    end
-    
-    context "inline w/ gaJsHost syntax", :vcr => { :cassette_name => "blackhatworld_com" } do
-      let(:ga) { described_class.new(page_body("http://www.blackhatworld.com/index.php")) }
-      
-      it { ga.output[:found].should eq true }
-      it { ga.output[:ua].should eq "UA-24324223-1" }
     end
 
     context "no google analytics" do
