ad37be347ec1824134f97c6dd16d1d9702186d13
Ezekiel Templin <zeke@templ.in>
2013-12-29 03:26:35 -0800

    Update AdSense sniffer
    NOTE: Google advertising should probably be handled collectively.

--- lib/sniffles/sniffers/advertising/adsense.rb
+++ lib/sniffles/sniffers/advertising/adsense.rb
@@ -2,28 +2,30 @@ module Sniffles
   module Sniffers
     class Adsense
       include Text
-      
+
       attr_accessor :doc
       attr_reader :output
-      
+
       def initialize(response_body)
-        @output = {}        
+        @output = {}
         parse(response_body) && process_document
       end
-      
+
       def process_document
         if @output[:found] = found?
           parse_publisher_id
         end
       end
-      
+
       private
       def found?
-        match? /googleAddAdSenseService/
+        match?(/googletag\.pubads\(\)/)
       end
-      
+
       def parse_publisher_id
-        @output[:publisher_id] = capture(/googleAddAdSenseService\(["|']+([a-z]+\-pub\-[\d]+)["|']+\)/)
+        if pub_id = capture(/publisher\:\s\"([a-z0-9\-]{10,})\"/)
+          @output[:publisher_id] = pub_id
+        end
       end
     end
   end
--- spec/sniffles/sniffers/advertising/adsense_spec.rb
+++ spec/sniffles/sniffers/advertising/adsense_spec.rb
@@ -1,18 +1,18 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/advertising/adsense')
+require 'spec_helper'
+require 'sniffles/sniffers/advertising/adsense'
 
 describe Sniffles::Sniffers::Adsense do
-  describe "#output" do    
-    context "found", :vcr => { :cassette_name => "squidoo_com" } do
-      let(:adsense)  { described_class.new(page_body("http://www.squidoo.com")) }
-      
+  describe "#output" do
+    context "found", :vcr => { :cassette_name => "digital-photography-school_com" } do
+      let(:adsense)  { described_class.new(page_body("http://digital-photography-school.com/digital-photography-tips-for-beginners")) }
+
       it { adsense.output[:found].should eq true }
-      it { adsense.output[:publisher_id].should eq "ca-pub-9879162776784828" }
+      it { adsense.output[:publisher_id].should =~ /[a-z0-9\-]{10,}/ }
     end
 
     context "not found" do
       let(:blank)   { described_class.new(empty_html_doc) }
-      
+
       it { blank.output[:found].should eq false }
     end
   end
