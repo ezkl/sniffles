b5f909fcea9e946b1f0948bc36b839f15889395c
Ezekiel Templin <zeke@templ.in>
2013-12-29 04:12:29 -0800

    Clean vBulletin sniffer

--- lib/sniffles/sniffers/cms/vbulletin.rb
+++ lib/sniffles/sniffers/cms/vbulletin.rb
@@ -2,31 +2,31 @@ module Sniffles
   module Sniffers
     class Vbulletin
       include HTML
-      
+
       attr_accessor :doc
       attr_reader :output
-      
+
       def initialize(response_body)
-        @output = {}        
+        @output = {}
         parse(response_body) && process_document
       end
-      
+
       def process_document
         if @output[:found] = found?
           parse_version
           parse_feed
         end
       end
-      
+
       private
       def found?
         !!(js_initialization? || meta_generator_content =~ /vBulletin/)
       end
-      
+
       def parse_feed
         @output[:feed] = text_at("//link[@type='application/rss+xml']/@href")
       end
-      
+
       def parse_version
         if meta_generator_content
           @output[:version] = (meta_generator_content.match(/([\d]+\.[\d]+\.[\d]+)/) ? $1 : false)
@@ -34,11 +34,11 @@ module Sniffles
           @output[:version] = false
         end
       end
-      
+
       def meta_generator_content
         text_at("//meta[@name='generator']/@content")
       end
-      
+
       def js_initialization?
         @doc.xpath("//script[contains(text(),'vBulletin_init()')]").any?
       end
--- spec/sniffles/sniffers/cms/vbulletin_spec.rb
+++ spec/sniffles/sniffers/cms/vbulletin_spec.rb
@@ -1,27 +1,21 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/cms/vbulletin')
+require 'spec_helper'
+require 'sniffles/sniffers/cms/vbulletin'
 
 describe Sniffles::Sniffers::Vbulletin do
-  describe "#output"  do    
-    context "vbulletin w/ meta generator", :vcr => { :cassette_name => "vbulletin_com" } do
-      let(:vbulletin) { described_class.new(page_body("https://www.vbulletin.com/forum/forum.php")) }
-      
+  describe "#output"  do
+    context "vbulletin w/ meta generator and JS", :vcr => { :cassette_name => "blackhatworld_com" } do
+      let(:vbulletin) { described_class.new(page_body("http://www.blackhatworld.com/")) }
+
       it { vbulletin.output[:found].should eq true }
-      it { vbulletin.output[:version].should eq "4.1.11" }
-      it { vbulletin.output[:feed].should eq "https://www.vbulletin.com/forum/external.php?type=RSS2" }
-    end
-    
-    context "vbulletin w/ JS initialization", :vcr => { :cassette_name => "blackhatworld_com" } do
-      let(:vbulletin) { described_class.new(page_body("http://www.blackhatworld.com/index.php")) }
-      
+      it { vbulletin.output[:version].should eq "4.2.1" }
+      it { vbulletin.output[:feed].should eq "http://www.blackhatworld.com/blackhat-seo/external.php?type=RSS2" }
       it { vbulletin.output[:found].should eq true }
     end
-    
+
     context "not vbulletin" do
       let(:blank) { described_class.new(empty_html_doc) }
-      
+
       it { blank.output[:found].should eq false }
     end
-    
   end
 end
