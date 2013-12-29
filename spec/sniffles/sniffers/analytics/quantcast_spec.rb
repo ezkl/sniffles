724fcec374adec9096ad614d8bdc721097207f77
Ezekiel Templin <zeke@templ.in>
2013-12-29 04:46:01 -0800

    Clean Quantcast sniffer

--- spec/sniffles/sniffers/analytics/quantcast_spec.rb
+++ spec/sniffles/sniffers/analytics/quantcast_spec.rb
@@ -1,17 +1,17 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/analytics/quantcast')
+require 'spec_helper'
+require 'sniffles/sniffers/analytics/quantcast'
 
 describe Sniffles::Sniffers::Quantcast do
   describe "#output" do
-    context "quantcast", :vcr => { :cassette_name => 'squidoo_com' } do
-      let(:quantcast) { described_class.new(page_body("http://www.squidoo.com")) }
-      
+    context "quantcast", :vcr => { :cassette_name => 'stackoverflow_com' } do
+      let(:quantcast) { described_class.new(page_body("http://stackoverflow.com/")) }
+
       it { quantcast.output[:found].should eq true }
     end
 
     context "no quantcast" do
       let(:blank) { described_class.new(empty_html_doc) }
-      
+
       it { blank.output[:found].should eq false }
     end
   end
