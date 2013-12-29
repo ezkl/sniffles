6ac045483e32b6416cf3548b37e2b0f710dd2897
Ezekiel Templin <zeke@templ.in>
2013-12-29 04:47:21 -0800

    Fix jQuery sniffer spec (this needs work)

--- spec/sniffles/sniffers/javascript/jquery_spec.rb
+++ spec/sniffles/sniffers/javascript/jquery_spec.rb
@@ -1,17 +1,17 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../../../lib/sniffles/sniffers/javascript/jquery')
+require "spec_helper"
+require 'sniffles/sniffers/javascript/jquery'
 
 describe Sniffles::Sniffers::Jquery do
-  describe "#output" do    
-    context "jquery", :vcr => { :cassette_name => "squidoo_com" } do
-      let(:jquery)  { described_class.new(page_body("http://www.squidoo.com")) }
-      
+  describe "#output" do
+    context "jquery", :vcr => { :cassette_name => "blog_mixpanel_com" } do
+      let(:jquery)  { described_class.new(page_body("http://blog.mixpanel.com/")) }
+
       it { jquery.output[:found].should eq true }
     end
 
     context "no jquery" do
       let(:blank)   { described_class.new(empty_html_doc) }
-      
+
       it { blank.output[:found].should eq false }
     end
   end
