94bd551917387935c05c1433dc35590b181c1f7c
Ezekiel Templin <zeke@templ.in>
2013-12-28 23:35:05 -0800

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
-require File.expand_path(File.dirname(__FILE__) + '/../lib/sniffles')
-require File.expand_path(File.dirname(__FILE__) + '/../spec/support/helpers')
+require 'bundler/setup'
+Bundler.setup(:default, :test)
+
+require 'sniffles'
 require 'vcr'
+
+require 'support/helpers'
+require 'rspec/autorun'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
@@ -12,5 +17,4 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  config.extend VCR::RSpec::Macros  
 end
